7fabcfd3e6aa3c9bd67b752a7eb7985edd42340c
Naomi-Dennis <njdennis94@gmail.com>
2018-04-25 21:09:50 -0400

    Finished Projects Added NAv

--- js/main.js
+++ js/main.js
@@ -3,10 +3,21 @@ function backToTop() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+function navHoverEffect(e) {
+    $(e.target.parentNode.firstChild).css("color", "rgba(0, 0, 0, 1)")
+}
+
+function navHoverEffectOut(e) {
+    $(e.target.parentNode.firstChild).css("color", "rgba(165, 165, 165, 255)");
+
+}
+
 $(document).ready(
     init
 )
 
 function init() {
     $("ol.carousel-indicators li").on("click", backToTop)
+    $("nav ul li a").on("mouseover", navHoverEffect);
+    $("nav ul li a").on("mouseleave", navHoverEffectOut);
 }
